db99d834a0bce20550adcbbbda8eaa19411c3ca3
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-04-30 08:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let var fname = document.getElementById('fname')
+	let var lname = document.getElementById('lname')
+	alert(fname+" "+lname)
 }
